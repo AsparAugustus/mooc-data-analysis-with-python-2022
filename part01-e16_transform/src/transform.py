5e3d433f3e968891902de1bfe09063590f75156c
Calvin Chan <xLysergia@gmail.com>
2022-12-16 11:48:26 +0000

    completed

--- part01-e16_transform/src/transform.py
+++ part01-e16_transform/src/transform.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 
-def transform(s1, s2):
-    return []
+def transform(*args):
+
+    newList = []
+    for arg in args:
+        arg = arg.split()
+        newList.append(arg)
+        
+
+
+
+    zipped = zip(*newList)
+
+  
+
+    s = list(map(lambda x : int(x[0]) * int(x[1]), zipped))
+
+    print(s)
+
+
+
+    return s
 
 def main():
+    transform("1 5 3", "2 6 -1") 
+
     pass
 
 if __name__ == "__main__":
