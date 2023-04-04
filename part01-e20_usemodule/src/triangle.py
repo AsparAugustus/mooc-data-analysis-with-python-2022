54c5f219a79340f1e2f01d5e3523e128996a081f
Calvin Chan <xLysergia@gmail.com>
2023-04-04 16:11:48 +0100

    usermodle completed

--- part01-e20_usemodule/src/triangle.py
+++ part01-e20_usemodule/src/triangle.py
@@ -1,1 +1,34 @@
+import math
+
 # Enter you module contents here
+
+__version__ = '1.0'
+__author__ = 'AsparAugustus'
+
+def hypotenuse(side1, side2):
+    """
+    Calculate the length of the hypotenuse of a right-angled triangle.
+
+    Args:
+        side1 (float): The length of one of the other sides.
+        side2 (float): The length of the other other side.
+
+    Returns:
+        float: The length of the hypotenuse.
+    """
+    c = math.sqrt(side1 ** 2 + side2**2)
+    return c
+
+def area(base, height):
+    """
+    Calculate the area of a right-angled triangle.
+
+    Args:
+    base (float): The length of the base.
+    height (float): The length of the height.
+
+    Returns:
+    float: The area of the triangle.
+    """
+    area = (base*height)/2
+    return area
--- part01-e20_usemodule/src/usemodule.py
+++ part01-e20_usemodule/src/usemodule.py
@@ -9,5 +9,18 @@ except ModuleNotFoundError:
 def main():
     # Call the functions from here
 
+    # Call the hypotenuse function
+    side1 = 3
+    side2 = 4
+    hypotenuse_length = triangle.hypotenuse(side1, side2)
+    print(f'The hypotenuse length is {hypotenuse_length:.2f}')
+
+    # Call the area function
+    base = 5
+    height = 10
+    area = triangle.area(base, height)
+    print(f'The area of the triangle is {area:.2f}')
+
+
 if __name__ == "__main__":
     main()
